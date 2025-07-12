e7302183f4b7aa79b0a4146de3b5dceed9f5519e
NazarDushka <dushkanazar@gmail.com>
2025-07-12 16:15:06 +0200

    Created Simple Main Menu
    Start, options, credits, exit buttons work
    -No shadows, no animations

---
+++ Hare_s_Blooming_Lace/Assets/Main Menu/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Quitting");
+        Application.Quit();
+    }
+
+}
